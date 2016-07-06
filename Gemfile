c911fced609713a9c28524008aae884a0d4e661b
Mason <lantay@alumni.unc.edu>
2016-07-06 19:35:57 +0000

    submit form

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
 gem 'sass-rails'
+gem "will_paginate", "~> 3.0.4" 
+gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,16 @@
 class PlacesController < ApplicationController
 
   def index
-    @places= Place.all
+     @places = Place.paginate(:page => params[:page], :per_page => 10)   
+
+       respond_to do |format|
+        format.html  #index.html.erb
+        format.json { render json: @places }
+     end 
   end
+
+  def new
+    @place=Place.new
+  end
+
 end
