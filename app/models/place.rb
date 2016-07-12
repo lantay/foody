bcbaf9a369d194f146dd8a43f5f74eeac69ba42f
Mason <lantay@alumni.unc.edu>
2016-07-12 17:37:38 +0000

    comment database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ActiveRecord::Base
 
-  belongs_to :user     
+  belongs_to :user
+  has_many :comments
+       
   geocoded_by :address
   after_validation :geocode
   validates :name, presence: true, length:{minimum:3}
