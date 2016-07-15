11d00adcf2e63c23763bd123a52b14f4a440ac24
Mason <lantay@alumni.unc.edu>
2016-07-15 16:21:16 +0000

    trying to squash heroku email bug

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@foodyapp.com"
 
-  def comment_added(comment)
-    @place=comment.place
-    @place_owner=@place.user
-    mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
+def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+         subject: "A comment has been added to #{@place.name}")
   end
 end
