e946fdfe04f34bcc69cf3eb20c02e90a266e893a
Mason <lantay@alumni.unc.edu>
2016-07-15 16:23:21 +0000

    removed auto email to try and fix heroku

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -15,7 +15,7 @@ class Comment < ActiveRecord::Base
     RATINGS.invert[self.rating]
   end
 
-  def send_comment_email
-    NotificationMailer.comment_added(self).deliver
-  end
+  # def send_comment_email
+  #   NotificationMailer.comment_added(self).deliver
+  # end
 end
